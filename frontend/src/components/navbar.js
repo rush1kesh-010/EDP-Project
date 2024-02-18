446b022bbef9305d13388beaadcd7007448745ee
bhakti0725 <balansebhakti0703@gmail.com>
2024-02-18 14:20:03 +0530

    navbar changes

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,25 +1,36 @@
 import {useState } from 'react';
 import '../styles/navbar.css';
+import '../global.css';
 //The code should be modular with ES6 import/export syntax. Make separate file for each component and import them in App.js. Make sure the resultant code is responsive, use bootstrap or SCSS for responsiveness.
+
+
+
 const navbar = () => {
 
 
     return (
-        <div className='navbar'>
-            <nav>
-                <img src='/images/logo.png' alt='logo' />
-                <ul>
-                    <li>Home</li>
-                    <li>Our Products</li>
-                    <li>Doctor's Profile</li>
-                </ul>
-                <div class='user'>
-                    <h3>Simon</h3>
-                    <img src='/images/user.png' alt='user' />
-                </div>
-                </nav>
+        <header className="navbar">
+        <div className="edp-wrapper">
+          <b className="edp">EDP</b>
         </div>
+        <div className="home-page-frame">
+          <div className="box">{`Home `}</div>
+          <div className="box">Notes</div>
+          <div className="box">Doctors web</div>
+          <div className="box">Log in</div>
+        </div>
+        <div className="text-label">
+          <div className="nurse-detail"><b>Darrell Steward</b></div>
+          <img
+            className="main-menu-bar"
+            loading="eager"
+            alt=""
+            src="../assets/Ellipse 10.png"
+          />
+        </div>
+      </header>
     )
 }
+
 
 export default navbar;
